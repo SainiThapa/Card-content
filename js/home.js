1b479b90250b4d30c1166bc39c7626b54b9b7a12
SainiThapa <magarprisu4747@gmail.com>
2024-04-10 23:16:44 +0545

    new commit

--- js/home.js
+++ js/home.js
@@ -2,23 +2,17 @@ console.log("loaded js")
 // var content = document.querySelector(".card-content");
 var seeMoreButtons=document.querySelectorAll('.seeMoreButton');
 // var content = this.previousElementSibling;
-var seebelow=document.querySelector('.seemore')
-
 // seeMoreButton.addEventListener('click',function(event){
-console.log(seebelow);
 seeMoreButtons.forEach(button=>{
     button.addEventListener('click',function(){
 
         var card=this.closest('.card');
         var content=card.querySelector('.card-content')
-
-        if (content.style.display === 'none' || !content.style.display) {
+        if (content.style.display === 'none' || content.style.display=="") {
             content.style.display = 'block';
-            this.textContent = 'See Less ';
-            seebelow.style.display=="none";
+            this.textContent = 'See Less';
         } else {
             content.style.display = 'none';
-            seebelow.style.display=="block";
             this.textContent = 'See More';
         }
     });
